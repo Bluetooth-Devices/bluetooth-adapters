1cf0ef5bffe52588d95564dbb77a0129de026ba5
Joseph Gilby <josephgilbyiv@gmail.com>
2024-10-31 01:34:12 +0000

    use isdigit()

--- src/bluetooth_adapters/dbus.py
+++ src/bluetooth_adapters/dbus.py
@@ -74,9 +74,7 @@ def _adapters_from_managed_objects(
         # check that path is exactly /org/bluez/hci<integer>
         if not path_str.startswith("/org/bluez/hci"):
             continue
-        try:
-            int(path_str[14:])
-        except ValueError:
+        if not path_str[15:].isdigit():
             continue
         split_path = path_str.split("/")
         adapter = split_path[3]
